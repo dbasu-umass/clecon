ea0ce3ae77d089e0f63a8249906c5710bfe1d95b
Deepankar Basu <dbasu@umass.edu>
2023-02-18 11:18:02 -0500

    Two new functions

---
+++ ppnewint3.R
@@ -0,0 +1,78 @@
+# Function to compute the vector of 
+# labor values, the vector of 
+# price of production and the uniform 
+# rate of profit for the circulating capital 
+# model using the New Interpretation
+# Marx's labor theory of value and
+# allowing for wage differential across
+# industries
+
+ppnewint3 <- function(A, Ap, l, lp, w, v, Q, Qp){
+  
+  # -- Inputs
+  # A (nxn): input output matrix
+  # Ap (mxm): input output matrix for productive sectors
+  # l (1Xn): direct labor vector
+  # lp (1Xm): direct labor vector for productive sectors
+  # w: average wage rate (scalar)
+  # v: value of labor power (scalar)
+  # Q (nx1): gross output vector
+  # Qp (mx1): gross output vector for productive sectors
+  
+
+  if(v>=(lp%*%Qp)/(l%*%Q)){
+    stop("Uniform rate of profit cannot be computed")
+  } else{
+    
+    # Identity matrix 
+    I <- diag(ncol(A))
+    Ip <- diag(ncol(Ap))
+    
+    # Net output
+    y <- (I-A)%*%Q
+    
+    # -- Maximum eigenvalue of A
+    maxEigenv <- max(Mod(eigen(A)$values))
+    
+    # -- Maximum rate of profit
+    R <- (1/maxEigenv)-1
+    
+    
+    # ----- Solve for uniform rate of profit
+    
+    # -- Define Univariate Function of rate of profit
+    myfunc <- function(r2){
+      
+      return(
+        (1+r2)*l%*%(solve(I-(1+r2)*A))%*%y - (lp%*%Qp)/v
+      )
+    }
+    
+    # Find root to get uniform rate of profit
+    # Note: upper bound should be kept less than
+    # R because the function blows up at R
+    r <- uniroot(myfunc,c(0,R-0.01))$root
+    
+    # ----- Solve for price of production vector
+    p_abs <- (1+r)*(w*l)%*%solve(I-(1+r)*A)
+    
+    # Vector of values 
+    lambda <- lp%*%solve(Ip - Ap)
+    
+    # MEV
+    mev <- (p_abs%*%y)/(lp %*%Qp)
+    
+    
+    return(list("Max Eigen Value (A)" = maxEigenv,
+                "Maximal Rate of Profit" = R,
+                "Uniform Rate of Profit" = r,
+                "Prices of Production (Absolute)" = p_abs,
+                "Prices of Production (Relative)" = p_abs/p_abs[1],
+                "Values" = lambda,
+                "Monetary Expression of Value" = mev[1,1])
+    )
+  }
+  
+  
+}
+
---
+++ ppnewint4.R
@@ -0,0 +1,92 @@
+# Function to compute the vector of 
+# labor values, the vector of 
+# price of production and the uniform 
+# rate of profit for the circulating capital 
+# model using the New Interpretation
+# Marx's labor theory of value and
+# allowing for wage differential across
+# industries and some unproductive sectors
+
+ppnewint4 <- function(A, Ap, l, lp, w, wp, v, Q, Qp){
+  
+  # -- Inputs
+  # A (nxn): input output matrix
+  # Ap (mxm): input output matrix for productive sectors
+  # l (1Xn): direct labor vector
+  # lp (1Xm): direct labor vector for productive sectors
+  # w (1xn): vector of wage rates
+  # wp (1xm): vector of wage rates for productive sectors
+  # v: value of labor power (scalar)
+  # Q (nx1): gross output vector
+  # Qp (mx1): gross output vector for productive sectors
+  
+  A <- res_data$Ahat
+  Ap <- res_data$Aphat
+  l <- res_data$l
+  lp <- res_data$lp
+  w <- res_data$wagevector_all
+  wp <- res_data$wagevector
+  v <- res_data$vlp
+  Q <- res_data$Q
+  Qp <- res_data$Qp
+  
+  if(v>=(lp%*%(diag(wp))%*%Qp)/(l%*%(diag(w))%*%Q)){
+    stop("Uniform rate of profit cannot be computed")
+  } else{
+    
+    # Identity matrix 
+    I <- diag(ncol(A))
+    Ip <- diag(ncol(Ap))
+    
+    # Net output
+    y <- (I-A)%*%Q
+    
+    # -- Maximum eigenvalue of A
+    maxEigenv <- max(Mod(eigen(A)$values))
+    
+    # -- Maximum rate of profit
+    R <- (1/maxEigenv)-1
+    
+    
+    # ----- Solve for uniform rate of profit
+    
+    # -- Define Univariate Function of rate of profit
+    myfunc <- function(r2){
+      
+      return(
+        (1+r2)*(l%*%diag(w))%*%(solve(I-(1+r2)*A))%*%y - ((lp%*%diag(wp))%*%Qp)/v
+      )
+    }
+    
+    # Find root to get uniform rate of profit
+    # Note: upper bound should be kept less than
+    # R because the function blows up at R
+    r <- uniroot(myfunc,c(0,R-0.01))$root
+    
+    # ----- Solve for price of production vector
+    p_abs <- (1+r)*(l%*%diag(w))%*%solve(I-(1+r)*A)
+    
+    # Vector of values 
+    lambda <- lp%*%solve(Ip - Ap)
+    
+    # MEV
+    mev <- (p_abs%*%y)/(lp %*%Qp)
+    
+    # Monetary expression of value (using gross output)
+    mev_gross <- (p_abs%*%Q)/(lambda%*%Qp)
+    
+    
+    return(list("Max Eigen Value (A)" = maxEigenv,
+                "Maximal Rate of Profit" = R,
+                "Uniform Rate of Profit" = r,
+                "Prices of Production (Absolute)" = p_abs,
+                "Prices of Production (Relative)" = p_abs/p_abs[1],
+                "Values" = lambda,
+                "Monetary Expression of Value" = mev[1,1],
+                "Monetary Expression of Value (Gross)" = mev_gross[1,1])
+    )
+    
+  }  
+  
+  
+}
