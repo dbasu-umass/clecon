d9973edb8da31f509871ae779107693bc1954573
Deepankar Basu <dbasu@umass.edu>
2023-02-26 22:02:57 -0500

    Added PF conditions

--- ppnewint3.R
+++ ppnewint3.R
@@ -32,6 +32,11 @@ ppnewint3 <- function(A, Ap, l, lp, w, v, Q, Qp){
     
     # -- Maximum eigenvalue of A
     maxEigenv <- max(Mod(eigen(A)$values))
+    # Is A nonnegative?
+    nn_A <- ifelse(min(A)>=0,1,0)
+    # Is A irreducible?
+    require(popdemo)
+    ir_A <- ifelse(popdemo::isIrreducible(A),1,0)
     
     # -- Maximum rate of profit
     R <- (1/maxEigenv)-1
@@ -68,7 +73,10 @@ ppnewint3 <- function(A, Ap, l, lp, w, v, Q, Qp){
                 "Prices of Production (Absolute)" = p_abs,
                 "Prices of Production (Relative)" = p_abs/p_abs[1],
                 "Values" = lambda,
-                "Monetary Expression of Value" = mev[1,1])
+                "Monetary Expression of Value" = mev[1,1],
+                "A: Nonnegative (1=Y,0=N)" = nn_A,
+                "A: Irreducible (1=Y,0=N)" = ir_A
+                )
     )
   }
   
